9851cd91ac00d29db984ab086b2bf4d172610e52
LadyAudrey <theaudreygreene@gmail.com>
2022-10-06 11:54:24 -0700

    adding eslint adding JS to noEAplus15

--- WordGames/noEAplus15/noEAplus15.js
+++ WordGames/noEAplus15/noEAplus15.js
@@ -1,5 +1,4 @@
 // Program to find all the words with no A's or E's and are at least 15 letters long
-//not currently working
 // Importing the fs module
 let fs = require("fs")
 
@@ -14,11 +13,12 @@ let words = readFileLines('/home/evergreen/UnderDogsAssignments/sowpods');
 noEA = []
 noEAPlus15 = []
 
+for(let i = 0; i < words.length; i++) { 
+    if (!words[i].includes("E") && !words[i].includes("A")) {
+        noEA.push(words[i]);}}
+
 for(let j = 0; j < noEA.length; j++) { 
     if (noEA[j].length >= 15 ) {
         noEAPlus15.push(noEA[j]);
-        console.log(noEA[j]);
-    }
-}
-
-console.log(noEAPlus15)
+    }}
+console.log("The words containing no E's or A's and at least 15 letters long are " + noEAPlus15)
