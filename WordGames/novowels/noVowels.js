4f4d4908070a116e9c79a2967a48a53a6700c21d
LadyAudrey <theaudreygreene@gmail.com>
2022-10-10 18:34:12 -0700

    updating Js for noVowels to find shortest word

--- WordGames/novowels/noVowels.js
+++ WordGames/novowels/noVowels.js
@@ -19,3 +19,13 @@ for(let i = 0; i < words.length; i++) {
 
 
 console.log("The words containing no vowels are " + noVowels)
+
+//setting variables to compare index in noVowels
+let longestNoVowels = "SH";
+for (let j = 0; j < noVowels.length; j++) {
+    if (noVowels[j].length > longestNoVowels.length) {
+        longestNoVowels = noVowels[j];
+    } else {
+        continue
+    }}
+console.log("The longest word with no vowels is " + longestNoVowels)
