f09b7b4856516cc6c48671fee083d164dbdcee77
LadyAudrey <theaudreygreene@gmail.com>
2022-10-12 18:01:09 -0700

    fixing file names and duplicates in palindromes

--- WordGames/palindrome/palindromes.js
+++ WordGames/palindrome/palindromes.js
@@ -14,6 +14,7 @@ let words = readFileLines('/home/evergreen/UnderDogsAssignments/sowpods');
 let palindromes = []
 let longestPalindrome = "AA";
 let howLong = longestPalindrome.length
+console.log("line 17 " + Number.isInteger(howLong))
 
 // Cycle through words and find all the palindromes by comparing last letter for first letters
 for (let i = 0; i < words.length - 1; i++) {
@@ -25,13 +26,12 @@ for (let i = 0; i < words.length - 1; i++) {
 
 // Find the longest index in palindrome
 for (let j = 0; j < words.length; j++) {
-    if (typeof(palindromes[j]) != undefined) {
-        if (palindromes[j].length > howLong) {
-            longestPalindrome = palindromes[j];
-            console.log("this is longestPalindrome " + longestPalindrome);
+    let pIndex = String(palindromes[j])
+        howLong = longestPalindrome.length;
+        if (pIndex.length > howLong) {
+            longestPalindrome = pIndex;
         }
-    } 
 }
 
-//console.log("All the words that are palindromes are " + palindromes)
-//console.log("The longest palindrome is " + longestPalindrome)
+console.log("All the words that are palindromes are " + palindromes)
+console.log("The longest palindrome is " + longestPalindrome)
